7a50dd7cf99d04872acd76dba005b620d27f1a0b
Alex <sheppenguin@gmail.com>
2024-01-15 11:24:48 -0500

    added framework for Firebase, Login & Register page as for MS2

--- server.js
+++ server.js
@@ -3,15 +3,33 @@ var express = require('express');
 var fs = require('fs');
 var cookieParser = require('cookie-parser');
 
+// Firebase imports
+const { initializeApp } = require('firebase/app');
+const { getDatabase } = require('firebase/database');
+
 var app = express();
 app.use(cookieParser());
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChG3rvO-2dWLqzaxQS2BVSfG953CJXJUg",
+  authDomain: "tej4mculminating.firebaseapp.com",
+  projectId: "tej4mculminating",
+  storageBucket: "tej4mculminating.appspot.com",
+  messagingSenderId: "951928473492",
+  appId: "1:951928473492:web:2d5303a38572c243b2f181",
+  measurementId: "G-8YSK128KTF"
+};
+
+// Initialize Firebase
+const fbapp = initializeApp(firebaseConfig);
+const database = getDatabase(fbapp);
+
 function send(res, contentType, fileName) {
-    //console.log(`Attempting to send file: ${fileName}`); // Add this line for debugging
     res.set('Content-Type', contentType);
     fs.readFile(fileName, (err, data) => {
         if (err) {
-            console.error(`Error reading the file: ${err.message}`); // Add this line for debugging
+            console.error(`Error reading the file: ${err.message}`);
             res.status(500).send('Error reading the file');
         } else {
             res.send(data);
@@ -48,6 +66,16 @@ app.get('/script.js', (req, res) => {
 app.get('/animation.js', (req, res) => {
     send(res, 'application/javascript', 'animation.js');
 })
+
+app.get('/Login/login.html', (req, res) => {
+    send(res, 'text/html', 'Login/login.html');
+})
+
+app.get('/login.css', (req, res) => {
+    send(res, 'text/css', 'Login/login.css');
+})
+
+
 
 console.log('Listening');
 app.listen(5500);
